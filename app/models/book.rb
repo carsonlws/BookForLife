d40eef05a646b31dfe1d9c71eb8083a85b298085
Carson Leong <carsonleong0930@hotmail.com>
2016-09-03 09:57:17 +0800

    Done elasticsearch

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,13 +3,9 @@ class Book < ApplicationRecord
   has_many :ordered_books
   has_many :orders, through: :ordered_books
   belongs_to :category
+
   searchkick
 
-  products = Product.search "apples"
-  products.each do |product|
-    puts product.name
-  end
-
   mount_uploader :image, ImageUploader
-
+  
 end
